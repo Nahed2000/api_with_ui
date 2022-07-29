05a19ba2199b13d0a4435370a43577e0b37d319a
Nahed Oukal <87930585+Nahed2000@users.noreply.github.com>
2022-07-29 22:26:52 +0300

    image & upload image ui

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute:'/home_screen',
+      initialRoute:'/login_screen',
         routes:
         {
           '/lunch_screen':(context) =>const LunchScreen(),
