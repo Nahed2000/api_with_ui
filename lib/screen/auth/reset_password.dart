01f9a14a221363fedfc1f21251ff9b96888cb851
Nahed Oukal <87930585+Nahed2000@users.noreply.github.com>
2022-07-29 20:16:50 +0300

    reset screen api & ui

---
+++ lib/screen/auth/reset_password.dart
@@ -0,0 +1,128 @@
+import 'package:api_review/api/api_controller/auth_api_controller.dart';
+import 'package:api_review/model/api_response.dart';
+import 'package:api_review/util/helper.dart';
+import 'package:api_review/widget/custom_button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../widget/custom_text_field.dart';
+
+class ResetPassword extends StatefulWidget {
+  const ResetPassword({Key? key,required this.email}) : super(key: key);
+ final String email ;
+  @override
+  State<ResetPassword> createState() => _ResetPasswordState();
+}
+
+class _ResetPasswordState extends State<ResetPassword> with Helper {
+  late TextEditingController _emailController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _emailController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text(
+          'Reset Password',
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              letterSpacing: 2,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric (horizontal: 20,vertical: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Forget Password ...!!',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                letterSpacing: 1.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              'please enter your email to sent a code  ',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 16,
+                letterSpacing: 1,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const SizedBox(height: 40),
+            CustomTextField(
+              controller: _emailController,
+              hintText: 'Email',
+              prefixIcon: Icons.email,
+            ),
+            const SizedBox(height: 20),
+            CustomButton(
+                onPress: () async => await _performForget(), title: 'Sent a Code'),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have account?"),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Register Now!'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _performForget() async {
+    if (checkData()) {
+      await login();
+    }
+  }
+
+  bool checkData() {
+    if (_emailController.text.isNotEmpty) {
+      return true;
+    }
+    showSnackBar(context,
+        message:
+            'Required Data, Please Enter your Email ',
+        error: true);
+    return false;
+  }
+
+  Future<void> login() async {
+    ApiResponse apiResponse = await AuthApiController().forget(
+        email: _emailController.text);
+
+    showSnackBar(context,
+        message: apiResponse.message, error: !apiResponse.status);
+     if (apiResponse.status) {
+      Navigator.pushReplacementNamed(context, '/home_screen');
+    }
+  }
+}
